c78511d67c3683f8c4f0661846f8cf6d540b5816
Jelili Adesina <jelilio.adesina@gmail.com>
2024-12-17 02:44:16 +0000

    initial commit

---
+++ src/test/java/io/github/jelilio/demopaypal/DemoPaypalApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jelilio.demopaypal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoPaypalApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
